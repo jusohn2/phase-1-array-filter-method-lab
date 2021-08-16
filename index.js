917274d0cffb8af9e82934e24f233f77ccd2468d
Justin Sohn <jusohn2015@gmail.com>
2021-08-15 22:50:35 -0400

    commit tree 4fa437cfd7220ab365ef6b911a214b9dc80e7bcf

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(name,string){
+    for(i=0; i<name.length; i++){
+        if(i != string){
+            return false; 
+        }
+    }
+    return name;
+}
+drivers.filter(findMathcing);
